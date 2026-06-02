5f512db21d32e803875878bdfb9a1967441d9934
camposnic12-web <camposnic12@gmail.com>
2026-06-02 14:06:51 -0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+// Reemplazá estos dos valores con los tuyos de Supabase → Settings → API
+const SUPABASE_URL = 'https://gjltwunzpcedppqahtdi.supabase.co'
+const SUPABASE_ANON_KEY = 'sb_publishable_Ywn45Mfz2YrHhn8AjtkKbQ_m_cj1wji'
+
+const { createClient } = supabase
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
