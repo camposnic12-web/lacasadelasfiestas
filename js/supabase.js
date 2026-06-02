df07ca00eeee7f475b5e0b2e5151e43680bd575d
camposnic12-web <camposnic12@gmail.com>
2026-06-02 15:18:34 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,5 @@
-// Reemplazá estos dos valores con los tuyos de Supabase → Settings → API
 const SUPABASE_URL = 'https://jgltwunzpcedppqahtdi.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_Ywn45Mfz2YrHhn8AjtkKbQ_m_cj1wji'
+const SUPABASE_ANON_KEY = 'sb_publishable_Ywn4SMFz2YrHhn8AjtkKbQ_m_cj1wji'
 
 const { createClient } = supabase
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
