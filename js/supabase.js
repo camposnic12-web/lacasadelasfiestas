e1e9d5be5c4d8dcc58b0ab6c0bc827950a041a75
poolstore <camposnic12@gmail.com>
2026-06-28 11:38:41 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://jgltwunzpcedppqahtdi.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_Ywn4SMFz2YrHhn8AjtkKbQ_m_cj1wji'
+const SUPABASE_ANON_KEY = 'sb_publishable_Ywn45Mfz2YrHhn8AjtkKbQ_m_cj1wji'
 
 const { createClient } = supabase
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
